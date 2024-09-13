9109fe52ae158172e80c83708c190812ed119bf5
Melissa Choy <mel@melchoy.com>
2024-09-14 08:43:07 +1000

    combine install/uninstall to one file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-
 source ~/.dotfiles/common.sh
-sh $DOTMANGR_PLATFORM_DIR/install.sh
-sh $DOTMANGR_BASE_DIR/install/vault.sh
-sh $DOTMANGR_BASE_DIR/install/dots.sh
+
+do_install() {
+	sh $DOTMANGR_PLATFORM_DIR/install.sh
+	sh $DOTMANGR_BASE_DIR/install/vault.sh
+	sh $DOTMANGR_BASE_DIR/install/dots.sh
+}
+
+do_uninstall () {
+	sh $DOTMANGR_PLATFORM_DIR/uninstall.sh
+	sh $DOTMANGR_BASE_DIR/install/dots.sh --uninstall
+}
+
+if [ "$1" == "--uninstall" ]; then
+	do_uninstall
+else
+	do_install 
+fi
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/.dotfiles/common.sh
-sh $DOTMANGR_PLATFORM_DIR/uninstall.sh
-sh $DOTMANGR_BASE_DIR/install/dots.sh --uninstall
