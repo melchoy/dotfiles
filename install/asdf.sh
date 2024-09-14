3b07151942c9fb0b5de01f9fbd802dc37200afc2
Melissa Choy <mel@melchoy.com>
2024-09-15 06:41:26 +1000

    asdf

--- install/asdf.sh
+++ install/asdf.sh
@@ -7,37 +7,33 @@ if [ ! -d "$HOME/.asdf" ]; then
   git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch v0.14.1
 fi
 
-# Source ASDF scripts
 source $HOME/.asdf/asdf.sh
-
-# Ensure ASDF is available
 if command -v asdf &> /dev/null; then
 
   # Define plugin installation check
-  is_plugin_installed() {
+  is_asdf_plugin_installed() {
     local plugin_name="$1"
     asdf plugin list | grep -q "^$plugin_name" &> /dev/null
   }
 
   # Install plugin if not already installed
-  install_plugin() {
+  install_asdf_plugin() {
     local plugin_name="$1"
     local plugin_repo="$2"
-    if ! is_plugin_installed "$plugin_name"; then
+    if ! is_asdf_plugin_installed "$plugin_name"; then
       echo "Installing $plugin_name plugin..."
       asdf plugin add "$plugin_name" "$plugin_repo"
-    else
-      echo "$plugin_name plugin already installed."
     fi
   }
 
   # Install required plugins
-  install_plugin nodejs
-  install_plugin ruby
-  install_plugin elixir
+  install_asdf_plugin nodejs
+  install_asdf_plugin ruby
+	install_asdf_plugin erlang
+  install_asdf_plugin elixir
 
   # Install a default version if no version is installed
-  ensure_default_version() {
+  install_asdf_package_version() {
     local lang="$1"
     local latest_version
     if [[ -z "$(asdf list "$lang")" ]]; then
@@ -45,13 +41,12 @@ if command -v asdf &> /dev/null; then
       latest_version=$(asdf latest "$lang")
       asdf install "$lang" "$latest_version"
       asdf global "$lang" "$latest_version"
-    else
-      echo "$lang already has versions installed."
     fi
   }
 
   # Ensure default versions are installed
-  ensure_default_version nodejs
-  ensure_default_version ruby
-  ensure_default_version elixir
+  install_asdf_package_version nodejs
+  install_asdf_package_version ruby
+	install_asdf_package_version erlang
+  install_asdf_package_version elixir
 fi
