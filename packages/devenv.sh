903c2b30d9370430c02886e0de27d0f56272d376
Melissa Choy <mel@melchoy.com>
2024-09-16 12:49:07 +1000

    install go

--- packages/devenv.sh
+++ packages/devenv.sh
@@ -10,17 +10,18 @@ if [ ! -d "$HOME/.asdf" ]; then
 fi
 
 source $HOME/.asdf/asdf.sh
-if command -v asdf &> /dev/null; then
-	symlink_dotfile "$DOTMANGR_CONFIGS_DIR/.asdfrc"
+symlink_dotfile "$DOTMANGR_CONFIGS_DIR/.asdfrc"
 
-  install_asdf_plugin ruby
-	install_asdf_package_version ruby
+# TODO: Implement Optional Selections
 
-	install_asdf_plugin erlang
-	install_asdf_package_version erlang
+install_asdf_plugin ruby
+install_asdf_package_version ruby
 
-  install_asdf_plugin elixir
-	install_asdf_package_version elixir
+install_asdf_plugin erlang
+install_asdf_package_version erlang
 
-	sh $DOTMANGR_DEV_PACKAGES_DIR/node.sh
-fi
+install_asdf_plugin elixir
+install_asdf_package_version elixir
+
+sh $DOTMANGR_DEV_PACKAGES_DIR/node.sh
+sh $DOTMANGR_DEV_PACKAGES_DIR/golang.sh
---
+++ packages/devenv/golang.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ~/.dotfiles/common.sh
+
+if [[ "$PLATFORM_NAME" == "mac" ]]; then
+	brew install golang
+elif [[ "$PLATFORM_NAME" == "ubuntu" ]]; then
+	echo "GOLANG INSTALLER NOT YET IMPLEMENTED FOR UBUNTU"
+	echo "Update this installer here ~/.dotfiles/packages/devenv/golang.sh to add ubuntu support"
+fi
