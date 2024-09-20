c6516a8253cdc86f0f6819c392878f91d9714873
Melissa Choy <mel@melchoy.com>
2024-09-20 21:27:47 +1000

    restore vscode installer and symlinks

--- packages/vscode.sh
+++ packages/vscode.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source ~/.dotfiles/common.sh
 
 extension_list=(
@@ -31,5 +33,30 @@ install_vscode_extensions() {
     fi
   done
 }
+
+if [[ "$PLATFORM_NAME" == "mac" ]]; then
+  install_or_update_package "visual-studio-code"
+
+  # Symlink configuration files
+  symlink_dotfile ~/.dotfiles/configs/vscode/macos/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+  symlink_dotfile ~/.dotfiles/configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+  symlink_dotfile ~/.dotfiles/configs/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+
+elif [[ "$PLATFORM_NAME" == "ubuntu" ]]; then
+  if ! dpkg -l | grep -q "code"; then
+    echo "VSCode not found, adding repository and installing..."
+
+    # Add Microsoft repository and GPG key
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+  fi
+
+  install_or_update_package "code"
+
+  symlink_dotfile ~/.dotfiles/configs/vscode/ubuntu/keybindings.json ~/.config/Code/User/keybindings.json
+  symlink_dotfile ~/.dotfiles/configs/vscode/settings.json ~/.config/Code/User/settings.json
+  symlink_dotfile ~/.dotfiles/configs/vscode/snippets ~/.config/Code/User/snippets
+fi
 
 install_vscode_extensions
