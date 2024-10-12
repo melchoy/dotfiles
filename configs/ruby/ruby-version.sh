78df6e3181587d401001f70187d4479d163caccb
Melissa Choy <mel@melchoy.com>
2024-10-12 21:18:29 +1100

    revert to default ruby outside project dir

--- configs/ruby/ruby-version.sh
+++ configs/ruby/ruby-version.sh
@@ -1,29 +1,43 @@
+find-ruby-version-file() {
+  local dir="$PWD"
+  while [ "$dir" != "/" ]; do
+    if [ -f "$dir/.ruby-version" ]; then
+      echo "$dir/.ruby-version"
+      return
+    fi
+    dir=$(dirname "$dir")
+  done
+}
+
 load-ruby-version() {
   if ! command -v rbenv &> /dev/null; then
     echo "rbenv is not installed. Please install rbenv first."
     return
   fi
 
-  if [ -f .ruby-version ]; then
-    local ruby_version=$(cat .ruby-version)
+  local ruby_version_file=$(find-ruby-version-file)
+  if [ -n "$ruby_version_file" ]; then
+    local ruby_version=$(cat "$ruby_version_file")
     if ! rbenv versions --bare | grep -q "^${ruby_version}$"; then
       echo "Ruby version $ruby_version is not installed."
       echo -n "Would you like to install it now? (y/N) "
       read response
       if [[ "$response" =~ ^[Yy]$ ]]; then
         rbenv install "$ruby_version"
-        rbenv global "$ruby_version"
-        echo "Ruby version $ruby_version has been installed and set as global."
+        rbenv shell "$ruby_version"
+        echo "Now using ruby version to $ruby_version."
       else
         echo "Ruby version $ruby_version is not installed."
         echo "If you would like to run this project"
-				echo "please install it using the following command:"
+        echo "please install it using the following command:"
         echo "rbenv install $ruby_version"
       fi
     else
-      rbenv global "$ruby_version"
-      echo "Ruby version $ruby_version is already installed and set as global."
+      rbenv shell "$ruby_version"
+      echo "Now using ruby version to $ruby_version."
     fi
+  else
+    rbenv shell --unset
   fi
 }
 
