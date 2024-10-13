f345b7c7c1dec98d263fe0a9e97b969789eb0418
Melissa Choy <mel@melchoy.com>
2024-10-13 17:55:29 +1100

    create directories and nvim opener

--- config/zsh/functions/utils.zsh
+++ config/zsh/functions/utils.zsh
@@ -1,9 +1,20 @@
 
 # Create a directory or file if it doesn't exist
-nf() {
-	if [[ "$1" == */* ]]; then
+function nf() {
+	if [[ "$1" == */ ]]; then
+		mkdir -p "$1"
+	elif [[ "$1" == */* ]]; then
 		mkdir -p "$(dirname "$1")" && touch "$1"
 	else
 		touch "$1"
 	fi
+}
+
+# vim -> open vim in the current directory or open the target file
+function vim() {
+  if [[ $# -eq 0 ]]; then
+      nvim .
+  else
+    nvim "$@"
+  fi
 }
