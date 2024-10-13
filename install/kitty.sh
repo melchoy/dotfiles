a1e448e9161d35b15ec093c52b162397951b6745
Melissa Choy <mel@melchoy.com>
2024-10-13 13:53:20 +1100

    dont launch kitty

--- install/kitty.sh
+++ install/kitty.sh
@@ -2,6 +2,6 @@
 
 source ~/.dotfiles/common.sh
 
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 
 symlink_dotfile "$DOTMANGR_CONFIGS_DIR/kitty" "$HOME/.config/kitty"
