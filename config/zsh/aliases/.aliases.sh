8346b3939b814851138ec840b55c4102ecf5a9b4
Melissa Choy <mel@melchoy.com>
2024-10-14 16:39:58 +1100

    dev alias

--- config/zsh/aliases/.aliases.sh
+++ config/zsh/aliases/.aliases.sh
@@ -4,5 +4,7 @@ alias evim="vim ~/.config/nvim"
 alias copilot="gh copilot"
 alias suggest="gh copilot suggest"
 
+alias dev="./bin/dev"
+
 source $DOTZSH/aliases/dotfiles.sh
 source $DOTZSH/aliases/directory.sh
