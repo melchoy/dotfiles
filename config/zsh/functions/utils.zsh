a2f5ed80d8cfe097919a7e95f9bde8d97feb94b1
Melissa Choy <mel@melchoy.com>
2024-10-18 14:45:16 +1100

    add xcmp and xmv to extend cp and mv utilities to create directories for destination path if they don't exist

--- config/zsh/functions/utils.zsh
+++ config/zsh/functions/utils.zsh
@@ -10,6 +10,45 @@ function nf() {
 	fi
 }
 
+# Extend mv so that it creates the target path if it doesn't already exist
+xmv() {
+  if [ "$#" -lt 2 ]; then
+    echo "Usage: xmv [options] <source>... <target>"
+    return 1
+  fi
+
+  dest="${@: -1}"
+
+  if [[ "$dest" == */* ]]; then
+    dest_dir=$(dirname "$dest")
+
+    if [ ! -d "$dest_dir" ]; then
+      mkdir -p "$dest_dir"
+    fi
+  fi
+
+  mv "$@"
+}
+
+xcp() {
+  if [ "$#" -lt 2 ]; then
+    echo "Usage: xcp [options] <source>... <target>"
+    return 1
+  fi
+
+  dest="${@: -1}"
+
+  if [[ "$dest" == */* ]]; then
+    dest_dir=$(dirname "$dest")
+
+    if [ ! -d "$dest_dir" ]; then
+      mkdir -p "$dest_dir"
+    fi
+  fi
+
+  cp "$@"
+}
+
 # vim -> open vim in the current directory or open the target file
 function vim() {
   if [[ $# -eq 0 ]]; then
