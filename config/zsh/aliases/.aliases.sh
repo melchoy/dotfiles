4d7edd99c717d3870d35cf6981bb6b51dfb633bb
Melissa Choy <mel@melchoy.com>
2024-12-15 08:52:03 +1100

    python aliases

--- config/zsh/aliases/.aliases.sh
+++ config/zsh/aliases/.aliases.sh
@@ -6,5 +6,9 @@ alias suggest="gh copilot suggest"
 
 alias dev="./bin/dev"
 
+alias py="python3"
+alias python="python3"
+alias pip="pip3"
+
 source $DOTZSH/aliases/dotfiles.sh
 source $DOTZSH/aliases/directory.sh
