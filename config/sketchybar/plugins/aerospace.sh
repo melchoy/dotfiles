8a546adf9b9028a685248cfbe5ed5d87ec6b2aa5
Melissa Choy <mel@melchoy.com>
2025-10-08 09:41:32 +1100

    remove unused aerospace keybindings

--- config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/plugins/aerospace.sh
@@ -23,19 +23,20 @@ for i in {1..9}; do
   fi
 done
 
-# Check named workspaces (like X for Xtra/Last)
-for workspace in X; do
-  HAS_WINDOWS=$(aerospace list-windows --workspace "$workspace" 2>/dev/null)
+# Check workspace 0 (Last workspace)
+HAS_WINDOWS=$(aerospace list-windows --workspace "0" 2>/dev/null)
 
-  if [ "$workspace" = "$FOCUSED" ]; then
-    sketchybar --set space.$workspace drawing=on \
-                                      background.color=0xffc6a0f6 \
-                                      icon.color=0xff24273a
-  elif [ -n "$HAS_WINDOWS" ]; then
-    sketchybar --set space.$workspace drawing=on \
-                                      background.color=0xff5b6078 \
-                                      icon.color=0xffcad3f5
-  else
-    sketchybar --set space.$workspace drawing=off
-  fi
-done
+if [ "0" = "$FOCUSED" ]; then
+  # Focused workspace - show in purple (Catppuccin Macchiato mauve)
+  sketchybar --set space.0 drawing=on \
+                                background.color=0xffc6a0f6 \
+                                icon.color=0xff24273a
+elif [ -n "$HAS_WINDOWS" ]; then
+  # Has windows - show in surface1 gray
+  sketchybar --set space.0 drawing=on \
+                                background.color=0xff5b6078 \
+                                icon.color=0xffcad3f5
+else
+  # Empty and not focused - hide
+  sketchybar --set space.0 drawing=off
+fi
